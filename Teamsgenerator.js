8eaf6344480992aa201fc1b47072047e0d0f9a33
akhil1672 <33376520+akhil1672@users.noreply.github.com>
2017-11-17 22:59:52 +0530

    shuffling array

--- Teamsgenerator.js
+++ Teamsgenerator.js
@@ -73,9 +73,7 @@ function shuffle(array) {
     var i = array.length, j = 0, temp;
     while (i--) {
         j = Math.floor(Math.random() * (i + 1));
-        temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        array[i,j] = array[j,i];
     }
     return array;
 }
